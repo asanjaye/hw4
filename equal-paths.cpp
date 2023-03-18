27de19bc26042180d2e23c7ca13656d3052d336a
Ashwin Sanjaye <sanjaye@usc.edu>
2023-03-18 21:34:06 +0000

    part 1 done

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
+#include <iostream>
 #endif
 
 #include "equal-paths.h"
@@ -12,7 +12,36 @@ using namespace std;
 
 bool equalPaths(Node * root)
 {
+    if(root==nullptr){
+        return 1;
+    }
+    
     // Add your code below
+	
+    int leftPath = findHeight(root->left);
+    int rightPath = findHeight(root->right);
+    if(root->left==nullptr && root->right!=nullptr){
+        leftPath+= 1;
+    }
+    if(root->left!=nullptr && root->right==nullptr){
+        rightPath+= 1;
+    }
+    
+	
 
+    return (leftPath==rightPath);
 }
 
+int findHeight(Node* root){
+    
+    if(root==nullptr){
+			return 0;
+		}
+        
+		int path = findHeight(root->left) + findHeight(root->right) +1;
+        if(root->left !=nullptr && root->right !=nullptr){
+            path-=1;
+        }
+        
+		return path;
+}
--- equal-paths.h
+++ equal-paths.h
@@ -29,4 +29,6 @@ struct Node {
  */
 bool equalPaths(Node * root);
 
+int findHeight(Node* root);
+
 #endif
